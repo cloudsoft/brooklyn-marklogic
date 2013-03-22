92d6b6c5b918241730a85d9b22adb700679e22a7
Aled Sage <aled.sage@gmail.com>
2013-03-22 22:15:40 +0000

    Add missing annotation: MarkLogicCluster

--- src/main/java/io/cloudsoft/marklogic/MarkLogicCluster.java
+++ src/main/java/io/cloudsoft/marklogic/MarkLogicCluster.java
@@ -1,12 +1,14 @@
 package io.cloudsoft.marklogic;
 
 import brooklyn.entity.group.DynamicCluster;
+import brooklyn.entity.proxying.ImplementedBy;
 import brooklyn.event.AttributeSensor;
 import brooklyn.event.basic.BasicAttributeSensor;
 
 /**
  * Creates a cluster of MarkLogic nodes (where the first node is designated the master).
  */
+@ImplementedBy(MarkLogicClusterImpl.class)
 public interface MarkLogicCluster extends DynamicCluster {
 
 	public static final AttributeSensor<MarkLogicNode> MASTER_NODE = new BasicAttributeSensor<MarkLogicNode>(
