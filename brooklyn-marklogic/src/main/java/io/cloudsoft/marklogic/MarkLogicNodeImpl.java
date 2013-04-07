c11f12ab942ee522d5093fd180392f2ccf597d8f
Peter Veentjer <alarmnummer@gmail.com>
2013-04-07 11:09:47 +0300

    More updates on the installation script

--- brooklyn-marklogic/src/main/java/io/cloudsoft/marklogic/MarkLogicNodeImpl.java
+++ brooklyn-marklogic/src/main/java/io/cloudsoft/marklogic/MarkLogicNodeImpl.java
@@ -43,6 +43,8 @@ public class MarkLogicNodeImpl extends SoftwareProcessImpl implements MarkLogicN
     public void init(){
         //we give it a bit longer timeout for starting up
         setConfig(ConfigKeys.START_TIMEOUT,120);
+
+        //todo: ugly.. we don't want to get the properties in this way, but for the time being it works.
         setConfig(LICENSE_KEY, getManagementContext().getConfig().getFirst("brooklyn.marklogic.license-key"));
         setConfig(LICENSEE, getManagementContext().getConfig().getFirst("brooklyn.marklogic.licensee"));
     }
