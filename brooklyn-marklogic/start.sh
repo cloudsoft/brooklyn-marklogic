a3849af2875d2589c943483ba92886f991ae12ff
Andrea Turli <andrea.turli@gmail.com>
2013-04-09 20:15:03 +0200

    re-introduced reset_ml_on_startup on scripts

--- brooklyn-marklogic/start.sh
+++ brooklyn-marklogic/start.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-#export JAVA_OPTS=" -Dlogback.configurationFile=logback.xml -Xms256m -Xmx1g -XX:MaxPermSize=256m"
 export BROOKLYN_CLASSPATH=$(pwd)/target/classes
 brooklyn launch --app io.cloudsoft.marklogic.MarkLogicApp --location named:marklogic-us-east-1
+#brooklyn launch --app io.cloudsoft.marklogic.MarkLogicApp --location cloudservers-uk
+#brooklyn launch --app io.cloudsoft.marklogic.MarkLogicApp --location named:marklogic-eu-west-1
