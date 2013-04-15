54f33533b21e0bc97d94be9ef8f08062af82c9b4
Peter Veentjer <alarmnummer@gmail.com>
2013-04-15 14:34:34 +0300

    The BROOKLYN_MARKLOGIC_HOME variable is now inferred so scripts can be called from any location

--- dist/src/main/dist/bin/start-ec2.sh
+++ dist/src/main/dist/bin/start-ec2.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
-export BROOKLYN_MARKLOGIC_HOME=..
+if [ -z "${BROOKLYN_MARKLOGIC_HOME}" ] ; then
+    BROOKLYN_MARKLOGIC_HOME=$(cd $(dirname $(readlink -f $0 2> /dev/null || readlink $0 2> /dev/null || echo $0))/.. && pwd)
+fi
+
 export BROOKLYN_CLASSPATH=${BROOKLYN_MARKLOGIC_HOME}/lib/*
 brooklyn launch --app io.cloudsoft.marklogic.MarkLogicApp --location named:marklogic-us-east-1
--- dist/src/main/dist/bin/start.sh
+++ dist/src/main/dist/bin/start.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-export BROOKLYN_MARKLOGIC_HOME=..
+if [ -z "${BROOKLYN_MARKLOGIC_HOME}" ] ; then
+    BROOKLYN_MARKLOGIC_HOME=$(cd $(dirname $(readlink -f $0 2> /dev/null || readlink $0 2> /dev/null || echo $0))/.. && pwd)
+fi
+
 export JAVA_OPTS="-Dbrooklyn.catalog.url=${BROOKLYN_MARKLOGIC_HOME}/conf/marklogic-catalog.xml -Xms256m -Xmx1g -XX:MaxPermSize=256m"
 export BROOKLYN_CLASSPATH=${BROOKLYN_MARKLOGIC_HOME}/lib/*
 brooklyn launch
