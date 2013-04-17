d4293a9b5478df0cdcc528a0adbcbd8c9f3a6af7
Peter Veentjer <alarmnummer@gmail.com>
2013-04-17 13:02:44 +0300

    More logging cleanup

--- dist/src/main/dist/bin/start.sh
+++ dist/src/main/dist/bin/start.sh
@@ -5,5 +5,5 @@ if [ -z "${BROOKLYN_MARKLOGIC_HOME}" ] ; then
 fi
 
 export JAVA_OPTS="-Dbrooklyn.catalog.url=${BROOKLYN_MARKLOGIC_HOME}/conf/marklogic-catalog.xml -Xms256m -Xmx1g -XX:MaxPermSize=256m"
-export BROOKLYN_CLASSPATH=${BROOKLYN_MARKLOGIC_HOME}/lib/*
+export BROOKLYN_CLASSPATH=${BROOKLYN_MARKLOGIC_HOME}/conf/:${BROOKLYN_MARKLOGIC_HOME}/lib/*
 brooklyn launch
