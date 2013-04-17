7a7b181bf00988351ddbccbb6e79524a7f010b86
Peter Veentjer <alarmnummer@gmail.com>
2013-04-17 13:30:47 +0300

    - the logfiles are now written to the log dir - the dist has its own log configuration

--- dist/src/main/dist/bin/start-ec2.sh
+++ dist/src/main/dist/bin/start-ec2.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ -z "${BROOKLYN_MARKLOGIC_HOME}" ] ; then
-    BROOKLYN_MARKLOGIC_HOME=$(cd $(dirname $(readlink -f $0 2> /dev/null || readlink $0 2> /dev/null || echo $0))/.. && pwd)
+    export BROOKLYN_MARKLOGIC_HOME=$(cd $(dirname $(readlink -f $0 2> /dev/null || readlink $0 2> /dev/null || echo $0))/.. && pwd)
 fi
 
 export BROOKLYN_CLASSPATH=${BROOKLYN_MARKLOGIC_HOME}/conf/:${BROOKLYN_MARKLOGIC_HOME}/lib/*
--- dist/src/main/dist/bin/start.sh
+++ dist/src/main/dist/bin/start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ -z "${BROOKLYN_MARKLOGIC_HOME}" ] ; then
-    BROOKLYN_MARKLOGIC_HOME=$(cd $(dirname $(readlink -f $0 2> /dev/null || readlink $0 2> /dev/null || echo $0))/.. && pwd)
+    export BROOKLYN_MARKLOGIC_HOME=$(cd $(dirname $(readlink -f $0 2> /dev/null || readlink $0 2> /dev/null || echo $0))/.. && pwd)
 fi
 
 export JAVA_OPTS="-Dbrooklyn.catalog.url=${BROOKLYN_MARKLOGIC_HOME}/conf/marklogic-catalog.xml -Xms256m -Xmx1g -XX:MaxPermSize=256m"
