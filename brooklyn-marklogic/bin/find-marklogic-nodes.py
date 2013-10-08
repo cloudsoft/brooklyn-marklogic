43ae17fc9ed3207268ab69630ada6dd51c372c6f
Sam Corbett <sam.corbett@cloudsoftcorp.com>
2013-10-08 15:14:45 +0100

    Utility for automatic collection of MarkLogic logs
    - Requires python module requests (pip install requests)
    - Run ./collect-logs.sh

---
+++ brooklyn-marklogic/bin/find-marklogic-nodes.py
@@ -0,0 +1,44 @@
+import sys
+
+try:
+    import requests
+except ImportError:
+    print "Couldn't import requests. Have you run `sudo pip install requests`?"
+    sys.exit(1)
+
+def loadApps(entities=[]):
+    payload = {'items': ','.join(entities)}
+    return requests.get('http://localhost:8081/v1/applications/fetch', params=payload).json()
+
+def loadEntitySensors(entity):
+    appId = entity['applicationId']
+    entId = entity['id']
+    return requests.get('http://localhost:8081/v1/applications/'+appId+'/entities/'+entId+'/sensors/current-state').json()
+
+def isMarkLogicEntity(app):
+    return 'MarkLogic' in app['name']
+
+def isMarkLogicNode(app):
+    return app['type'] == 'io.cloudsoft.marklogic.nodes.MarkLogicNode'
+
+def walkEntities():
+    """Repeatedly calls loadApps until no new children are encountered"""
+    toCheck = ['']
+    visited = set()
+    
+    while len(toCheck) > 0:
+        checking = filter(lambda appId: appId not in visited, toCheck) 
+        checked  = loadApps(checking)
+        toCheck = []
+        for newApp in checked:
+            if newApp['id'] not in visited:
+                yield newApp
+                visited.add(newApp['id'])
+                if 'childrenIds' in newApp:
+                    toCheck.extend(newApp['childrenIds'])
+
+
+for entity in walkEntities():
+    if isMarkLogicNode(entity):
+        print loadEntitySensors(entity)['host.name']
+
