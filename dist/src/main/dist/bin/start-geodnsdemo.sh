c257ecdeba529b52dce0b4b07c8c375a8b8d6c80
Sam Corbett <sam.corbett@cloudsoftcorp.com>
2013-10-09 14:41:54 +0100

    Include lib/ on classpath (as well as lib/*) for startup scripts
    Otherwise demo-war is not found.

--- dist/src/main/dist/bin/start-geodnsdemo.sh
+++ dist/src/main/dist/bin/start-geodnsdemo.sh
@@ -3,7 +3,7 @@ if [ -z "${BROOKLYN_MARKLOGIC_HOME}" ] ; then
     export BROOKLYN_MARKLOGIC_HOME=$(cd $(dirname $(readlink -f $0 2> /dev/null || readlink $0 2> /dev/null || echo $0))/.. && pwd)
 fi
 
-export BROOKLYN_CLASSPATH=${BROOKLYN_MARKLOGIC_HOME}/conf/:${BROOKLYN_MARKLOGIC_HOME}/lib/*
+export BROOKLYN_CLASSPATH=${BROOKLYN_MARKLOGIC_HOME}/conf/:${BROOKLYN_MARKLOGIC_HOME}/lib/*:${BROOKLYN_MARKLOGIC_HOME}/lib/
 brooklyn launch --app io.cloudsoft.marklogic.brooklynapplications.GeoScalingMarkLogicDemoApplication --location aws-ec2:us-east-1,aws-ec2:us-east-1
 #brooklyn launch --app io.cloudsoft.marklogic.brooklynapplications.GeoScalingMarkLogicDemoApplication --location aws-ec2:eu-west-1,aws-ec2:ap-southeast-1,aws-ec2:us-west-1
 
